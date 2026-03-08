551a825f6c6975278af74dccba0ed27bd0d60fce
Benjamin Henri Hessler <hesslerbenjamin432@gmail.com>
2026-03-08 17:21:32 +0100

    Added Auto-Tiling

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,11 +1,25 @@
 extends CharacterBody2D
 class_name Player
 
+@export var tilemap: TileMapLayer
+@export var selected_tile: int = 1
+
 const SPEED := 400
-const JUMP_FORCE : float = 500.0
-const ACCELERATION : float = 1.0
-const DECELERATION : float = 1.0
+const JUMP_FORCE : float = 700.0
+const ACCELERATION : float = 0.1
+const DECELERATION : float = 0.1
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(delta: float) -> void:
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		var grid_pos = tilemap.local_to_map(tilemap.get_local_mouse_position())
+		
+		if event.button_index == MouseButton.MOUSE_BUTTON_LEFT:
+			tilemap.set_cell(grid_pos, selected_tile)
+		elif event.button_index == MouseButton.MOUSE_BUTTON_RIGHT:
+			tilemap.set_cell(grid_pos, -1)
+		print(grid_pos)
+	
+
+func _physics_process(delta: float) -> void:	
 	move_and_slide()
